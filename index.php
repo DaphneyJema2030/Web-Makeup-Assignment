6d53e48788892ca29200b3c5cc3c23a620885e2d
Daphneyjema2030 <daphney.jema@strathmore.edu>
2025-08-16 06:22:43 +0300

    sign in page

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial cale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>DBT-Makeup</title>
+</head>
+<body>
+    <div class="header">
+        Sign In
+    </div>
+
+    <form action="" method="post">
+    <input type="text" placeholder="Enter your fullname" required/>
+    <br>
+    <input type="text" placeholder="Enter your email" required/>
+    <br>
+    <input type="tel" placeholder="Enter your phone number" required/>
+    <br>
+    <input type="text" placeholder="Enter your username" required/>
+    <br>
+    <input type="password" placeholder="Enter your password" required />
+    <br>
+<br>
+    <input type="submit" value="Sign In" />
+    
+</form>
+
+
+</body>
+</html>
