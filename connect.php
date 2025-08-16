fd75fa0af8991e4c3b5f6b29b7f2ab748d7a52df
Daphneyjema2030 <daphney.jema@strathmore.edu>
2025-08-16 05:41:09 +0300

    connection

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+// connection.php
+require_once "constant.php";
+//Create conn
+$mysqli = new mysqli(Host_Name, DB_User, DB_Password, DB_Name);
+
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+?>
